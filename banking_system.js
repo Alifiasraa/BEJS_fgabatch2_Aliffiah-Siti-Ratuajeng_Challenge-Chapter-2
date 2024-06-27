24ba735e8461cb3b798eb56b719e18a4dbdf374b
Alifiasraa <ajeng1109@gmail.com>
2024-06-28 02:00:46 +0700

    add deposit and withdraw methods

---
+++ banking_system.js
@@ -0,0 +1,66 @@
+class BankingSystem {
+  constructor() {
+    this.balance = 1000000;
+  }
+
+  // methods
+  deposit(amount) {
+    this.amount = parseFloat(amount);
+    console.log(`Saldo sebelum ditambah Rp.${this.balance}`);
+    try {
+      if (!isNaN(this.amount) && this.amount > 0) {
+        this.balance += this.amount;
+        setTimeout(() => {
+          console.log(`Saldo setelah ditambah Rp.${this.balance}`);
+        }, 2000);
+        console.log("Loading...");
+      } else {
+        throw new SyntaxError("Jumlah saldo tidak valid.");
+      }
+    } catch (error) {
+      console.error(error.name);
+      console.error(error.message);
+    }
+  }
+
+  withdraw(amount) {
+    this.amount = parseFloat(amount);
+    console.log(`Saldo sebelum dikurangi Rp.${this.balance}`);
+    try {
+      if (!isNaN(amount) && amount > 0) {
+        if (amount <= this.balance) {
+          this.balance -= amount;
+          setTimeout(() => {
+            console.log(`Saldo setelah dikurangi Rp.${this.balance}`);
+          }, 2000);
+          console.log("Loading...");
+        } else {
+          throw new SyntaxError("Saldo tidak mencukupi.");
+        }
+      } else {
+        throw new SyntaxError("Jumlah saldo tidak valid.");
+      }
+    } catch (error) {
+      console.error(error.name);
+      console.error(error.message);
+    }
+  }
+}
+
+const account = new BankingSystem();
+
+// deposit
+document.getElementById("deposit").addEventListener("click", () => {
+  const addAmount = window.prompt(
+    "Masukkan jumlah saldo yang ingin ditambahkan:"
+  );
+  account.deposit(addAmount);
+});
+
+// withdraw
+document.getElementById("withdraw").addEventListener("click", () => {
+  const addAmount = window.prompt(
+    "Masukkan jumlah saldo yang ingin dikurangi:"
+  );
+  account.withdraw(addAmount);
+});
